e9883e347f98c8bc88bba028deac782b97c45ad5
aram <leandrojavr@gmail.com>
2020-07-27 12:37:52 -0300

    add overflow percentage test

--- tests/test_bar.c
+++ tests/test_bar.c
@@ -4,20 +4,44 @@
 #include <cmocka.h>
 #include <loading_bar.h>
 
+#define cmp_str(s1, s2) { \
+    for (int i = 0; s1[i] != 0; i++) { \
+        assert_true(s1[i] == s2[i]); \
+    } \
+}
+
 static void test_create_destroy(void **state) {
     loading_bar *b = loading_bar_create('#', 10);
     const char cmp[] = "[          ]";
 
-    for (int i = 0; i < sizeof(cmp) ;i++) {
-        assert_true(cmp[i] == b->bar[i]);
-    }
+    cmp_str(cmp, b->bar);
 
     loading_bar_destroy(b);
+}
+
+static void test_out_of_bounds_percentage(void **state) {
+    loading_bar *b = loading_bar_create('4', 20);
+    char cmp[] = "[44                  ]";
+    loading_bar_update(b, 10);
+
+    cmp_str(cmp, b->bar);
+
+    // Overflow value bar should be filled like 100%
+    loading_bar_update(b, 100);
+    for (size_t i = 1; i <= 20; i++) {
+        cmp[i] = '4';
+    }
+
+    cmp_str(cmp, b->bar);
+
+    loading_bar_destroy(b);
+
 }
 
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_create_destroy),
+        cmocka_unit_test(test_out_of_bounds_percentage),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
